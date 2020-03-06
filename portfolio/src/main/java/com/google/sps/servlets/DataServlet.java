3bd1d9b11d9a2988a4cd3e59541b14ec9d37fd94
Dipti Gupte <gupte.d50@gmail.com>
2020-03-05 21:35:24 -0800

    Use translate api to translate comments to Spanish

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -29,6 +29,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
   @Override
@@ -43,12 +52,13 @@ public class DataServlet extends HttpServlet {
       long id = entity.getKey().getId();
       String title = (String) entity.getProperty("title");
       long timestamp = (long) entity.getProperty("timestamp");
-
-      comments.add(title);
+      String languageCode = request.getParameter("languageCode");
+      String translatedText = (String) entity.getProperty("translatedText");
+      comments.add(translatedText);
     }
 
     Gson gson = new Gson();
-
+    response.setCharacterEncoding("UTF-8");
     response.setContentType("application/json;");
     response.getWriter().println(gson.toJson(comments));
   }
@@ -62,10 +72,21 @@ public class DataServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String title = request.getParameter("title");
     long timestamp = System.currentTimeMillis();
+    //String languageCode = request.getParameter("languageCode");
+    String languageCode = "es";
+    System.out.println(title);
+    System.out.println(languageCode);
 
     Entity commentEntity = new Entity("Comment");
     commentEntity.setProperty("title", title);
     commentEntity.setProperty("timestamp", timestamp);
+    // Do the translation.
+    Translate translate = TranslateOptions.getDefaultInstance().getService();
+    Translation translation =
+        translate.translate(title, Translate.TranslateOption.targetLanguage(languageCode));
+    
+    String translatedText = translation.getTranslatedText();
+    commentEntity.setProperty("translatedText",translatedText);
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(commentEntity);
